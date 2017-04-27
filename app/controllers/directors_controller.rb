f538eda494f74b5eb71e8ab70b23c20d0f9e3e18
Eddie Jou <eddiejou@users.noreply.github.com>
2017-04-27 18:51:30 -0500

    HW Changes Made

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,47 +1,51 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
+    render("/directors/index.html.erb")
   end
 
   def show
     @director = Director.find(params[:id])
+    render("/directors/show.html.erb")
   end
 
-  def new
+  def new_form
+    render("/directors/new_form.html.erb")
   end
 
   def create_row
-    @director = Director.new
-    @director.dob = params[:dob]
-    @director.name = params[:name]
-    @director.bio = params[:bio]
-    @director.image_url = params[:image_url]
+    director = Director.new
+    director.dob = params[:dob]
+    director.name = params[:name]
+    director.bio = params[:bio]
+    director.image_url = params[:image_url]
 
-    @director.save
+    director.save
 
-    render("show")
+    redirect_to("/directors")
   end
 
   def edit_form
     @director = Director.find(params[:id])
+    render("/directors/edit_form.html.erb")
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    director = Director.find(params[:id])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    director.dob = params[:dob]
+    director.name = params[:name]
+    director.bio = params[:bio]
+    director.image_url = params[:image_url]
 
-    @director.save
+    director.save
 
-    render("show")
+    redirect_to("/directors/#{director.id}")
   end
 
   def destroy
     @director = Director.find(params[:id])
-
     @director.destroy
+    render("/directors/destroy.html.erb")
   end
 end
--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,45 +1,49 @@
 class RolesController < ApplicationController
   def index
     @roles = Role.all
+    render("/roles/index.html.erb")
   end
 
   def show
     @role = Role.find(params[:id])
+    render("roles/show.html.erb")
   end
 
   def new_form
+    render("/roles/new_form.html.erb")
   end
 
   def create_row
-    @role = Role.new
-    @role.character_name = params[:character_name]
-    @role.movie_id = params[:movie_id]
-    @role.actor_id = params[:actor_id]
+    role = Role.new
+    role.character_name = params[:character_name]
+    role.movie_id = params[:movie_id]
+    role.actor_id = params[:actor_id]
 
-    @role.save
+    role.save
 
-    render("show")
+    redirect_to("/roles")
   end
 
   def edit_form
     @role = Role.find(params[:id])
+    render("/roles/edit_form.html.erb")
   end
 
   def update_row
-    @role = Role.find(params[:id])
+    role = Role.find(params[:id])
 
-    @role.character_name = params[:character_name]
-    @role.movie_id = params[:movie_id]
-    @role.actor_id = params[:actor_id]
+    role.character_name = params[:character_name]
+    role.movie_id = params[:movie_id]
+    role.actor_id = params[:actor_id]
 
-    @role.save
+    role.save
 
-    render("show")
+    redirect_to("/roles/#{role.id}")
   end
 
   def destroy
     @role = Role.find(params[:id])
-
     @role.destroy
+    render("roles/destroy.html.erb")
   end
 end
